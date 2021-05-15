4ce80fc1fe2034053fb678dd6afbcfc64f137f8a
roxy-codes <61820074+roxy-codes@users.noreply.github.com>
2021-05-15 16:50:42 +1200

    Minor changes

--- PDC_Assignment_2/src/com/online/shopping_gui/view/WelcomeView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/WelcomeView.java
@@ -16,34 +16,23 @@ import javax.swing.*;
  */
 public class WelcomeView extends JFrame {
 
-    private JButton custLogin, adminLogin, createAccount;
+    private MainMenuView mainMenu;
 
     public WelcomeView() {
         setLayout(null);
-        
-        ImageShopAndRun rightPanel = new ImageShopAndRun();
-        rightPanel.setBounds(20, 140, 400, 116);
-        this.add(rightPanel);
-        
-
-        custLogin = new JButton("Login as Customer");
-        adminLogin = new JButton("Login as Admin");
-        createAccount = new JButton("Create an Account");
-
         setPreferredSize(new Dimension(900, 600));
         setBackground(Color.white);
-        this.add(custLogin);
-        this.add(adminLogin);
-        this.add(createAccount);
 
-        custLogin.setBounds (450, 110, 150, 55);
-        adminLogin.setBounds (450, 185, 150, 55);
-        createAccount.setBounds (450, 260, 150, 55);
+        ImageShopAndRun rightPanel = new ImageShopAndRun();
+        rightPanel.setBounds(50, 200, 400, 116);
+        this.add(rightPanel);
+
+        mainMenu = new MainMenuView();
+        mainMenu.setBounds(475, 50, 400, 560);
+        this.add(mainMenu);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Login");
-
         WelcomeView hi = new WelcomeView();
         hi.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         hi.pack();
