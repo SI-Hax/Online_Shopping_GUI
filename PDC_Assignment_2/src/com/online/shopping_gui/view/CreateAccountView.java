efa0b900a30993505a30e37c2d08e3ad953bf07d
djorange77 <cenzltd2011@gmail.com>
2021-05-15 13:54:43 +1200

    -Added View package and CreateAccount class

---
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/CreateAccountView.java
@@ -0,0 +1,15 @@
+package com.online.shopping_gui.view;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Miguel Emmara - 18022146
+ * @author Amos Foong - 18044418
+ * @author Roxy Dao - 1073633
+ * @version 1.0
+ * @since 15/05/2021
+ */
+public class CreateAccountView extends JPanel {
+    
+}
