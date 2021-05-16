bdd521961a606e24cf3ff977c47442a2f6379140
roxeh <61820074+roxy-codes@users.noreply.github.com>
2021-05-16 13:39:29 +1200

    Added quit button

--- PDC_Assignment_2/src/com/online/shopping_gui/view/MainMenuView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/MainMenuView.java
@@ -13,7 +13,7 @@ import javax.swing.*;
  * @author roxyc
  */
 public class MainMenuView extends JPanel {
-    private JButton custLogin, adminLogin, createAccount;
+    private JButton custLogin, adminLogin, createAccount, quit;
 
     public MainMenuView(){
         setPreferredSize(new Dimension(400, 560));
@@ -22,14 +22,17 @@ public class MainMenuView extends JPanel {
         custLogin = new JButton("Login as Customer");
         adminLogin = new JButton("Login as Admin");
         createAccount = new JButton("Create an Account");
+        quit = new JButton("Quit");
 
         add(custLogin);
         add(adminLogin);
         add(createAccount);
+        add(quit);
 
         custLogin.setBounds(125, 110, 150, 55);
         adminLogin.setBounds(125, 185, 150, 55);
         createAccount.setBounds(125, 260, 150, 55);
+        quit.setBounds(125, 335, 150, 55);
     }
     
 }
