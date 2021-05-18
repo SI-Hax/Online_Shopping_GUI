880a5c19e451b8c0f1c80f4a547a4471fe06072f
roxy-codes <61820074+roxy-codes@users.noreply.github.com>
2021-05-18 20:32:55 +1200

    Add files via upload
    Added ProductsView and Table

---
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/Table.java
@@ -0,0 +1,34 @@
+package com.online.shopping_gui.view;
+
+import java.util.*;
+import javax.swing.table.AbstractTableModel;
+
+public class Table extends AbstractTableModel {
+
+    protected String[] column =  new String[]{"ID", "Product Name", "Price", "Category", "Stock"};
+    private Object[][] data;
+
+    public Table() {
+        this.column = column;
+        this.data = data;
+    }
+
+    @Override
+    public int getRowCount() {
+        return this.data.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.column.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return this.data[rowIndex][columnIndex];
+    }
+
+    public String getColumnName(int col) {
+        return column[col];
+    }
+}
