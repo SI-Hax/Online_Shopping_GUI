1c6f88e41cd8a1aa142bfde3b6eb6a6c7e6519d5
djorange77 <cenzltd2011@gmail.com>
2021-05-19 23:35:02 +1200

    -Merge features added by Miguel (Adding a back button into the bottom right corner).

--- PDC_Assignment_2/src/com/online/shopping_gui/view/LoginView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/LoginView.java
@@ -3,7 +3,6 @@ package com.online.shopping_gui.view;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * This class contains the Login GUI
@@ -11,12 +10,12 @@ import java.awt.*;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 2.0.1
+ * @version 2.0.2
  * @since 15/05/2021
  */
 public class LoginView extends JPanel {
 
-    private JButton okay, quit;
+    private JButton okay, quit, backBtn;
     private JLabel logLbl, passLbl;
     private JPasswordField enterPass;
     private JTextField loginTxt;
@@ -26,36 +25,31 @@ public class LoginView extends JPanel {
         this.setLayout(null);
 
         okay = new JButton("Login");
-        quit = new JButton("Quit");
+        quit = new JButton("Quit"); //TODO: Remove redundant Quit Button.
         logLbl = new JLabel("Login ID");
         passLbl = new JLabel("Password");
         enterPass = new JPasswordField(30);
         loginTxt = new JTextField(30);
-
-        //get the size of the screen
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = kit.getScreenSize();
-
-        this.setPreferredSize(new Dimension(400, 320));
-
-        //Calculate the frame location
-        int x = (screenSize.width - this.getWidth()) / 2;
-        int y = (screenSize.height - this.getHeight()) / 2;
-
-        this.setLocation(x, y);
+        backBtn = new JButton("Back");
 
         add(okay);
-        add(quit);
+        add(quit); //TODO: Remove redundant Quit Button.
         add(logLbl);
         add(passLbl);
         add(enterPass);
         add(loginTxt);
+        add(backBtn);
 
         okay.setBounds(75, 165, 100, 25);
-        quit.setBounds(180, 165, 100, 25);
+        quit.setBounds(180, 165, 100, 25); //TODO: Remove redundant Quit Button.
         logLbl.setBounds(80, 80, 100, 25);
         passLbl.setBounds(75, 120, 100, 25);
         enterPass.setBounds(145, 120, 100, 25);
         loginTxt.setBounds(145, 80, 100, 25);
+        backBtn.setBounds(280, 500, 100, 25);
+    }
+    
+    public JButton getBackBtn() {
+        return backBtn;
     }
 }
