be8d2a9a139329564b1cfa7056af3a46362e4bd9
djorange77 <cenzltd2011@gmail.com>
2021-05-19 23:46:44 +1200

    -Changed the new file path for importing data from the csv files.

--- PDC_Assignment_2/src/com/online/shopping_gui/utilities/ProductFileIO.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/utilities/ProductFileIO.java
@@ -25,13 +25,12 @@ import java.util.ArrayList;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 1.03
+ * @version 2.0.1
  * @since 10/04/2021
- *
  */
 public final class ProductFileIO {
 
-    public static final String PRODUCT_FILEPATH = "./resources/product_database.csv";
+    public static final String PRODUCT_FILEPATH = "./Resources/csv_files/product_database.csv";
 
     /**
      * This method reads Products' information from a .csv file and loads them
