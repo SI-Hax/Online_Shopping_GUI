4f07f660a01761abaceeb0f03c39e8ccce78bc2d
djorange77 <cenzltd2011@gmail.com>
2021-05-23 02:03:49 +1200

    -Create 3 separate JPanel to contain and group components (Table, Label, Buttons) with similar functionalities together. -Added getter methods for essential components that could be modified/cause modifications during run-time. -Amalgamate the 3 separate JPanels together to form the ShoppingCartView. -Test by adding the ShoppingCartView to a JFrame (set minimum size to the table's preferred size so information would not be obscured).

--- PDC_Assignment_2/src/com/online/shopping_gui/view/ShoppingCartView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/ShoppingCartView.java
@@ -5,13 +5,18 @@
  */
 package com.online.shopping_gui.view;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import com.online.shopping_gui.model.Customer;
 import com.online.shopping_gui.model.ProductList;
 import com.online.shopping_gui.model.ShoppingCart;
 import com.online.shopping_gui.model.Table;
 import com.online.shopping_gui.utilities.ProductFileIO;
 import java.awt.Dimension;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -24,26 +29,93 @@ import javax.swing.ScrollPaneConstants;
 public class ShoppingCartView extends JPanel
 {
     private final String[] COLUMN_HEADERS = new String[]{"Product", "Quantity", "Price"};
+    private ShoppingCart cart;
     private Table scTableModel;
     private JTable cartTable;
-    private ShoppingCart cart;
     private JScrollPane scrollPane;
+    private JPanel tablePanel, lblPanel, btnPanel;
+    private JLabel itemSelectedLbl;
+    private JButton rmvFromCartBtn, continueShopBtn, proceedToChkOutBtn;
     
     public ShoppingCartView(ShoppingCart cart) {
+        FlatLightLaf.install();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setPreferredSize(new Dimension(900, 500));
+        
         this.cart = cart;
+        
+        // Init table panel.
+        tablePanel = new JPanel();
         boolean sorter = true;
         this.scTableModel = new Table(cart.convertShoppingCart(), COLUMN_HEADERS);
-        
         cartTable = new JTable(scTableModel);
-        cartTable.setPreferredScrollableViewportSize(new Dimension(400, 400));
+        cartTable.setPreferredScrollableViewportSize(new Dimension(700, 400));
         cartTable.setFillsViewportHeight(true);
         cartTable.setAutoCreateRowSorter(sorter);
-
         scrollPane = new JScrollPane(cartTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        add(scrollPane);
+        tablePanel.add(scrollPane);
+        
+        // Init label panel to display item selected.
+        lblPanel = new JPanel();
+        itemSelectedLbl = new JLabel("Item Selected: ");
+        lblPanel.add(itemSelectedLbl);
+        
+        // Init bottom panel to allow user to add an item with quantity specified to cart.
+        btnPanel = new JPanel();
+        btnPanel.setLayout(new BoxLayout(btnPanel, BoxLayout.X_AXIS));
+        btnPanel.setPreferredSize(new Dimension(700, 40));
+        rmvFromCartBtn = new JButton("Remove From Cart");
+        continueShopBtn = new JButton("Continue Shopping");
+        proceedToChkOutBtn = new JButton("Proceed to Checkout");
+        btnPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+        btnPanel.add(rmvFromCartBtn);
+        btnPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+        btnPanel.add(continueShopBtn);
+        btnPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+        btnPanel.add(proceedToChkOutBtn);
+        btnPanel.add(Box.createRigidArea(new Dimension(20, 0)));
 
-        this.setPreferredSize(new Dimension(500, 500));
+        // Add all panels to the parent container.
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(tablePanel);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(lblPanel);
+        this.add(Box.createRigidArea(new Dimension(0, 5)));
+        this.add(btnPanel);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+    }
+
+    public ShoppingCart getCart() {
+        return cart;
+    }
+
+    public Table getScTableModel() {
+        return scTableModel;
+    }
+
+    public JTable getCartTable() {
+        return cartTable;
+    }
+
+    public JScrollPane getScrollPane() {
+        return scrollPane;
+    }
+
+    public JLabel getItemSelectedLbl() {
+        return itemSelectedLbl;
+    }
+
+    public JButton getRmvFromCartBtn() {
+        return rmvFromCartBtn;
+    }
+
+    public JButton getContinueShopBtn() {
+        return continueShopBtn;
+    }
+
+    public JButton getProceedToChkOutBtn() {
+        return proceedToChkOutBtn;
     }
     
     public static void main(String[] args) {
@@ -54,8 +126,10 @@ public class ShoppingCartView extends JPanel
         
         JFrame frame = new JFrame("Test Shopping Cart Table");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new ShoppingCartView(cart));
+        ShoppingCartView cartView = new ShoppingCartView(cart); 
+        frame.add(cartView);
         frame.pack();
+        frame.setMinimumSize(cartView.getPreferredSize()); // Specifies the min size so table's info wont be obscured.
         frame.setVisible(true);
     }
 }
