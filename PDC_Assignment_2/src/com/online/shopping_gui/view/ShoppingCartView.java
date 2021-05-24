eae43c664a5ca16b8782d770e8fbbab9d6935771
djorange77 <cenzltd2011@gmail.com>
2021-05-24 21:36:47 +1200

    -Removed some comments above the package.

--- PDC_Assignment_2/src/com/online/shopping_gui/view/ShoppingCartView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/ShoppingCartView.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.online.shopping_gui.view;
 
 import com.formdev.flatlaf.FlatLightLaf;
@@ -53,7 +48,7 @@ public class ShoppingCartView extends JPanel
         this.setBackground(Color.WHITE);
         
         this.cart = cart;
-        
+   
         // Init table panel.
         tablePanel = new JPanel();
         tablePanel.setBackground(Color.WHITE);
