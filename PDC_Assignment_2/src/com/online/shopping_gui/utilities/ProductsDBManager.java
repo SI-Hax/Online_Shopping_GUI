d6567172380a59d1469336b73d5206d8262e78e3
djorange77 <cenzltd2011@gmail.com>
2021-05-25 22:55:13 +1200

    -Added methods importData and exportData and their relevant helper methods to aid in saving/loading local data to/from the embedded database.

--- PDC_Assignment_2/src/com/online/shopping_gui/utilities/ProductsDBManager.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/utilities/ProductsDBManager.java
@@ -1,11 +1,16 @@
 package com.online.shopping_gui.utilities;
 
+import com.online.shopping_gui.enumerations.Category;
+import com.online.shopping_gui.model.Product;
+import com.online.shopping_gui.model.ProductList;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  * A Database Manager Class for managing an embedded 
@@ -22,7 +27,7 @@ public final class ProductsDBManager {
 
     private static final String USER_NAME = "pdc"; // DB username
     private static final String PASSWORD = "pdc"; // DB password
-    private static final String URL = "jdbc:derby:ProductsEDB; create=true"; // Embedded database for Products.
+    private static final String URL = "jdbc:derby:Products_EDB; create=true"; // Embedded database for Products.
     private static Connection connectionInstance;
 
     private ProductsDBManager() { }
@@ -32,8 +37,10 @@ public final class ProductsDBManager {
             try {
                 connectionInstance = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
                 System.out.println(URL + " CONNECTED....");
-            } catch (SQLException ex) {
-                System.err.println(ex.getMessage());
+            } catch (SQLException e) {
+                closeConnections();
+                JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
             }
         }
         return connectionInstance;
@@ -45,6 +52,7 @@ public final class ProductsDBManager {
                 connectionInstance.close();
             } catch (SQLException ex) {
                 System.err.println(ex.getMessage());
+                ex.printStackTrace();
             }
         }
     }
@@ -56,8 +64,10 @@ public final class ProductsDBManager {
         try {
             statement = getConnection().createStatement();
             resultSet = statement.executeQuery(sql);
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
         return resultSet;
     }
@@ -68,8 +78,10 @@ public final class ProductsDBManager {
         try {
             statement = getConnection().createStatement();
             return statement.executeUpdate(sql);
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
             return 0;
         }
     }
@@ -83,8 +95,10 @@ public final class ProductsDBManager {
                 String dropTable = "DROP TABLE "+ tableName;
                 updateDB(dropTable);
             } 
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
     }
     
@@ -92,11 +106,103 @@ public final class ProductsDBManager {
         try {
             DatabaseMetaData databaseMetadata = getConnection().getMetaData();
             ResultSet resultSet = databaseMetadata.getTables(null, null, tableName, null);
-            return resultSet.next(); 
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+            if(resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
         return false;
+    }
+    
+    public static void createTable() {
+        String createTbl = "CREATE TABLE PRODUCTS("
+                + "PRODUCTNAME VARCHAR(100), "
+                + "PRODUCTID INT(10), "
+                + "PRICE DOUBLE(10,2), "
+                + "CATEGORY VARCHAR(50), "
+                + "STOCK INT(10))";
+        
+        if(tableExists("PRODUCTS")) {
+            dropTableIfExists("PRODUCTS");
+        }
+        
+        updateDB(createTbl);
+    }
+    
+    public static ProductList importData() {
+        ProductList pList = new ProductList();
+        
+        String query = "SELECT * FROM PRODUCTS";
+        
+        try {
+            ResultSet rs = queryDB(query); // Query the Customer database for details.
+            String productName, category;
+            int productID, stock;
+            double price;
+            while(rs.next()) // While loop to pull data from resultString..
+            {
+                productName = rs.getString("PRODUCTNAME");
+                productID = rs.getInt("PRODUCTID");
+                price = rs.getDouble("PRICE");
+                category = rs.getString("PHONE");
+                stock = rs.getInt("STOCK");
+                pList.addSingleProduct(new Product(productName, productID, price, Category.valueOf(category), stock));
+            }
+        } catch(Exception e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+        return pList;
+    }
+    
+    public static boolean rowExists(String productName) {
+        String query = "SELECT * FROM PRODUCTS WHERE PRODUCTNAME = \'" + productName + "\'";
+        
+        try {
+            ResultSet rs = queryDB(query); // Query the admin database for a specific Customer.
+            
+            if(rs.next()) { // If rs.next has something...
+                return true; // Row exists.
+            }
+        } catch(Exception e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+        return false; // Row does not exist, hence, safe to add.
+    }
+    
+    public static void exportData(ProductList products) {
+        try {
+            if(!tableExists("PRODUCTS")) { // If table does not exist..
+                createTable(); // Create table.
+            }
+            
+            ArrayList<Product> pList = products.getProductList();
+            
+            for (Product product : pList) {
+                if(!rowExists(product.getProductName())) { // If the product is non-existent in the table...
+                    String insertValue = "INSERT INTO PRODUCTS VALUES (\'" 
+                            + product.getProductName() + "\', "         // Product name
+                            + product.getProductID() + ", "             // Product ID
+                            + product.getPrice() + ", \'"               // Price
+                            + product.getCategory().toString() + "\', " // Category
+                            + product.getStock() + ")";                 // Stock
+
+                    updateDB(insertValue);
+                } else {
+                    System.err.println("Product " + product.getProductName() + " exists!");
+                }
+            }
+        } catch(Exception e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
     }
     
     @Override // Override the Object clone method to prevent cloning
