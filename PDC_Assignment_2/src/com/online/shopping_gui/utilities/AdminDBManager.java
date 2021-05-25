0ddbad1d3e279f634421922bf1529842b97e5c47
djorange77 <cenzltd2011@gmail.com>
2021-05-25 22:54:59 +1200

    -Added methods importData and exportData and their relevant helper methods to aid in saving/loading local data to/from the embedded database.

--- PDC_Assignment_2/src/com/online/shopping_gui/utilities/AdminDBManager.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/utilities/AdminDBManager.java
@@ -1,14 +1,17 @@
-/*
- * The programs are designed for PDC paper
- */
 package com.online.shopping_gui.utilities;
 
+import com.online.shopping_gui.model.Administrator;
+import com.online.shopping_gui.model.User;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JOptionPane;
+
 
 /**
  * A Database Manager Class for managing an embedded 
@@ -18,14 +21,14 @@ import java.sql.Statement;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 2.0.1
+ * @version 2.1.0
  * @since 23/05/2021
  */
 public final class AdminDBManager {
 
     private static final String USER_NAME = "pdc"; // DB username
     private static final String PASSWORD = "pdc"; // DB password
-    private static final String URL = "jdbc:derby:AdminEDB; create=true"; // Embedded database for Admin.
+    private static final String URL = "jdbc:derby:Administrator_EDB; create=true"; // Embedded database for Admin.
 
     private static Connection connectionInstance;
 
@@ -36,8 +39,10 @@ public final class AdminDBManager {
             try {
                 connectionInstance = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
                 System.out.println(URL + " CONNECTED....");
-            } catch (SQLException ex) {
-                System.err.println(ex.getMessage());
+            } catch (SQLException e) {
+                closeConnections();
+                JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
             }
         }
         return connectionInstance;
@@ -49,6 +54,7 @@ public final class AdminDBManager {
                 connectionInstance.close();
             } catch (SQLException ex) {
                 System.err.println(ex.getMessage());
+                ex.printStackTrace();
             }
         }
     }
@@ -60,8 +66,10 @@ public final class AdminDBManager {
         try {
             statement = getConnection().createStatement();
             resultSet = statement.executeQuery(sql);
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
         return resultSet;
     }
@@ -72,8 +80,10 @@ public final class AdminDBManager {
         try {
             statement = getConnection().createStatement();
             return statement.executeUpdate(sql);
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
             return 0;
         }
     }
@@ -87,8 +97,10 @@ public final class AdminDBManager {
                 String dropTable = "DROP TABLE "+ tableName;
                 updateDB(dropTable);
             } 
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
     }
     
@@ -96,11 +108,103 @@ public final class AdminDBManager {
         try {
             DatabaseMetaData databaseMetadata = getConnection().getMetaData();
             ResultSet resultSet = databaseMetadata.getTables(null, null, tableName, null);
-            return resultSet.next(); 
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+            if(resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
         return false;
+    }
+    
+    public static void createTable() {
+        String createTbl = "CREATE TABLE ADMINISTRATOR("
+                + "LOGINID VARCHAR(50), "
+                + "PASSWORD VARCHAR(200), "
+                + "NAME VARCHAR(50), "
+                + "EMAIL VARCHAR(50))";
+        
+        if(tableExists("ADMINISTRATOR")) {
+            dropTableIfExists("ADMINISTRATOR");
+        }
+        
+        updateDB(createTbl);
+    }
+    
+    public static HashMap<String, User> importData() {
+        HashMap<String, User> administrators = new HashMap<>();
+        
+        String query = "SELECT * FROM ADMINISTRATOR";
+        
+        try {
+            ResultSet rs = queryDB(query); // Query the admin database for details.
+            String loginID, password, name, email;
+            
+            while(rs.next()) // While loop to pull data from resultString..
+            {
+                loginID = rs.getString("LOGINID");
+                password = rs.getString("PASSWORD");
+                name = rs.getString("NAME");
+                email = rs.getString("EMAIL");
+                administrators.put(loginID, new Administrator(loginID, Utilities.decrypt(password), name, email)); // Adds the admin to the hashmap.
+            }
+        } catch(Exception e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+        
+        return administrators;
+    }
+    
+    public static boolean rowExists(String loginID) {
+        String query = "SELECT * FROM ADMINISTRATOR WHERE LOGINID = \'" + loginID + "\'";
+        
+        try {
+            ResultSet rs = queryDB(query); // Query the admin database for a specific Admin.
+            
+            if(rs.next()) { // If rs.next has something...
+                return true; // Row exists.
+            }
+        } catch(Exception e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+        return false; // Row does not exist, hence, safe to add.
+    }
+    
+    public static void exportData(HashMap<String, User> users) {
+        try {
+            if(!tableExists("ADMINISTRATOR")) { // If table does not exist..
+                createTable(); // Create table.
+            }
+            
+            // Writes data from the passed in Hash Map onto the file specified.          
+            for (Map.Entry<String, User> user : users.entrySet()) {
+                if (user.getValue() instanceof Administrator) { // Check if user is an instance of an Administrator...
+                    String[] data = user.getValue().toString().split(",");
+
+                    if(!rowExists(data[0])) { // If the admin is non-existent in the table...
+                        String insertValue = "INSERT INTO ADMINISTRATOR VALUES (\'" 
+                                + data[0] + "\', \'" // Login ID
+                                + data[1] + "\', \'" // Encrypted Password
+                                + data[2] + "\', \'" // Name
+                                + data[3] + "\')";   // Email
+                        
+                        updateDB(insertValue);
+                    } else {
+                        System.err.println("User " + data[0] + " exists!");
+                    }
+                }
+            }
+        } catch(Exception e) {
+            closeConnections();
+            JOptionPane.showMessageDialog(null, e.getMessage(), ("Error: " + e.getClass().getSimpleName()), JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
     }
     
     @Override //Override the Object clone method to prevent cloning
