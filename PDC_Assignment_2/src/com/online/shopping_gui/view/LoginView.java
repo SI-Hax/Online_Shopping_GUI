1f82b01ee21a0dfd7560aa64f0eeb388ccad05c2
Miguel Emmara <m.miguelemmara@gmail.com>
2021-05-28 22:53:24 +1200

    Modify Login panel - Change Field size

--- PDC_Assignment_2/src/com/online/shopping_gui/view/LoginView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/LoginView.java
@@ -16,6 +16,10 @@ import javax.swing.*;
  */
 public class LoginView extends JPanel {
 
+    private final int TXT_FIELD_WIDTH = 135;
+    private final int TXT_FIELD_HEIGHT = 25;
+    private final int FIELD_CHARS = 30;
+
     private JButton okay, quit, backBtn;
     private JLabel logLbl, passLbl;
     private JPasswordField enterPass;
@@ -30,8 +34,8 @@ public class LoginView extends JPanel {
         quit = new JButton("Quit"); //TODO: Remove redundant Quit Button.
         logLbl = new JLabel("Login ID");
         passLbl = new JLabel("Password");
-        enterPass = new JPasswordField(30);
-        loginTxt = new JTextField(30);
+        enterPass = new JPasswordField(FIELD_CHARS);
+        loginTxt = new JTextField(FIELD_CHARS);
         backBtn = new JButton("Back");
 
         add(okay);
@@ -46,8 +50,8 @@ public class LoginView extends JPanel {
         quit.setBounds(180, 165, 100, 25); //TODO: Remove redundant Quit Button.
         logLbl.setBounds(80, 80, 100, 25);
         passLbl.setBounds(75, 120, 100, 25);
-        enterPass.setBounds(145, 120, 100, 25);
-        loginTxt.setBounds(145, 80, 100, 25);
+        enterPass.setBounds(145, 120, TXT_FIELD_WIDTH, TXT_FIELD_HEIGHT);
+        loginTxt.setBounds(145, 80, TXT_FIELD_WIDTH, TXT_FIELD_HEIGHT);
         backBtn.setBounds(280, 500, 100, 25);
     }
 
