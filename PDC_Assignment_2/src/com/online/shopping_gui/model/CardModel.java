533f415972692bad551071b8ae7bd8e5f9fd99fc
djorange77 <cenzltd2011@gmail.com>
2021-05-30 01:07:22 +1200

    -Added a few attributes (UserList, ProductList, ShoppingCart, boolean flags) so it can be used for multiple views. -Added helper methods to check login, create new customer account, and update database.

--- PDC_Assignment_2/src/com/online/shopping_gui/model/CardModel.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/model/CardModel.java
@@ -1,5 +1,10 @@
 package com.online.shopping_gui.model;
 
+import com.online.shopping_gui.utilities.AdminDBManager;
+import com.online.shopping_gui.utilities.CustomerDBManager;
+import com.online.shopping_gui.utilities.ProductsDBManager;
+import com.online.shopping_gui.utilities.Utilities;
+import java.util.NoSuchElementException;
 import java.util.Observable;
 
 /**
@@ -8,19 +13,140 @@ import java.util.Observable;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 2.0.1
+ * @version 2.1.0
  * @since 17/05/2021
  */
 public class CardModel extends Observable {
     protected int mainMenuSelection;
+    protected UserList users;
+    protected ProductList productList;
+    protected ShoppingCart shoppingCart;
+    protected boolean custLoginFlag, adminLoginFlag, createAccFlag, cardFlag;
     
     public CardModel() {
+        custLoginFlag = false;
+        adminLoginFlag = false;
+        createAccFlag = false;
+        cardFlag = false;
+//        users = new UserList(AdminDBManager.importData(), CustomerDBManager.importData()); 
+//        productList = ProductsDBManager.importData();
+        Runnable loadUsers = () -> {
+            users = new UserList(AdminDBManager.importData(), CustomerDBManager.importData()); 
+        };
+        new Thread(loadUsers).start();
         
+        Runnable loadProducts = () -> {
+            productList = ProductsDBManager.importData();
+        };
+        new Thread(loadProducts).start();
     }
     
-    public void setMainMenuSelection(int selection){
+      public void setMainMenuSelection(int selection){
         this.mainMenuSelection = selection;
+        cardFlag = true;
+        custLoginFlag = false;
+        adminLoginFlag = false;
+        createAccFlag = false;
         setChanged();
-        notifyObservers(mainMenuSelection);
+        notifyObservers(this);
+    }
+             
+    public boolean checkLogin(String loginID, String password) {
+        System.out.println("Checking login for " + loginID + ", " + password);
+        if(users.userExists(loginID)) { // If there's an existing user with passed-id login id...
+            System.out.println("User exists");
+            User user = users.searchUser(loginID);
+            if(user.getPassword().equals(password)) { // Check if password match database stored password.
+                System.out.println("Password match");
+                users.logInUser(loginID); // Log in user (saves it as current user).
+                if(user instanceof Customer) { // If user is an instance of Customer...
+                    shoppingCart = new ShoppingCart(user);
+                    System.out.println("Customer logged in.");
+                    custLoginFlag = true; // Set custLogin flag to true to enable customer pop-up window.
+                    cardFlag = false;
+                    adminLoginFlag = false;
+                    createAccFlag = false;
+                    setChanged();
+                    notifyObservers(this);
+                    return true;
+                } else if (user instanceof Administrator){ // Otherwise...
+                    adminLoginFlag = true; // Set adminLogin flag to true to enable admin pop-up window.
+                    cardFlag = false;
+                    custLoginFlag = false;
+                    createAccFlag = false;
+                    setChanged();
+                    notifyObservers(this);
+                    return true;
+                }
+            } else { 
+                throw new IllegalArgumentException("Incorrect password! Please try again.");
+            }
+        } else {
+            throw new NoSuchElementException("User (" + loginID + ") does not exist! Please use a valid login ID or create a new account.");
+        }
+        return false;
+    }
+    
+     public boolean addCustomer(String loginID, String password, String name, String phone, String email, String address, String cardNumber, String cardHolder) throws Exception { 
+        Customer newCustomer = new Customer(loginID, password, name, phone, email, address, cardNumber, cardHolder); 
+        boolean addSuccess = users.addUser(newCustomer);
+        updateCustDB(loginID, password, name, phone, email, address, cardNumber, cardHolder);
+        createAccFlag = true; // Set createAcc flag to true.
+        custLoginFlag = false; 
+        cardFlag = false;
+        adminLoginFlag = false;
+        setChanged();
+        notifyObservers(this);
+        return addSuccess;
+    }
+     
+    public void updateCustDB(String loginID, String password, String name, String phone, String email, String address, String cardNumber, String cardHolder) {
+        // Prepare statement to execute.
+        String updateDB = "INSERT INTO CUSTOMER VALUES (\'" 
+                            + loginID + "\', \'" // Login ID
+                            + Utilities.encrypt(password) + "\', \'" // Encrypted Password
+                            + name + "\', \'" // Name
+                            + phone + "\', \'" // Phone
+                            + email + "\', \'" // Email
+                            + address + "\', \'" // Address
+                            + Utilities.encrypt(cardNumber) + "\', \'" // Encrypted Card Number
+                            + cardHolder + "\')";   // Card Holder
+        
+        Runnable updateCustDB = () -> {
+            CustomerDBManager.updateDB(updateDB); // Insert row into table.
+        };
+        new Thread(updateCustDB).start(); // Run in separate thread (background) to optimize UI responsiveness.
+    }
+
+    public int getMainMenuSelection() {
+        return mainMenuSelection;
+    }
+    
+    public UserList getUsers() {
+        return users;
+    }
+    
+    public ProductList getProductList() {
+        return productList;
+    }
+    
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public boolean isCustLoginFlag() {
+        return custLoginFlag;
+    }
+
+    public boolean isAdminLoginFlag() {
+        return adminLoginFlag;
+    }
+
+    public boolean isCreateAccFlag() {
+        return createAccFlag;
+    }
+
+    public boolean isCardFlag() {
+        return cardFlag;
     }
 }
