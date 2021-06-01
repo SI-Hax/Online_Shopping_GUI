5932ba906d7b8200d3c682b39a2ce4c7c2f3b60a
djorange77 <cenzltd2011@gmail.com>
2021-06-02 08:16:44 +1200

    -Modified this class to be a JFrame and Observer so that it can be called later to pop-up during a button press. -Linked to the CardModel via the CardController. -Enlarged the text area for receipt/invoice so that more items can be displayed (if needed).

--- PDC_Assignment_2/src/com/online/shopping_gui/view/ReceiptView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/ReceiptView.java
@@ -1,15 +1,17 @@
 package com.online.shopping_gui.view;
 
 import com.formdev.flatlaf.FlatLightLaf;
+import com.online.shopping_gui.controller.CardController;
+import com.online.shopping_gui.model.CardModel;
 import com.online.shopping_gui.model.Customer;
 import com.online.shopping_gui.model.ProductList;
 import com.online.shopping_gui.model.ShoppingCart;
 import com.online.shopping_gui.model.User;
 import com.online.shopping_gui.utilities.ProductFileIO;
 import java.awt.*;
-import java.awt.event.*;
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.*;
-import javax.swing.event.*;
 
 /**
  * Contains fields to collect data from user.
@@ -17,45 +19,98 @@ import javax.swing.event.*;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 2.0.1
+ * @version 2.1.0
  * @since 25/05/2021
  */
-public class ReceiptView extends JPanel {
-
+public class ReceiptView extends JFrame implements Observer {
+    private JPanel mainPanel;
     private JTextArea receipt;
     private JLabel thank;
     private ShoppingCart cart;
+    private User currentUser;
 
-    public ReceiptView(ShoppingCart cart, User currentUser) {
+    public ReceiptView() {
+        super("Receipt");
         FlatLightLaf.install();
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         
-        this.cart = cart;
-        receipt = new JTextArea(20, 20);
+        initView();
+        
+        mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(284, 382));
+        mainPanel.setLayout(null);
+        
+        receipt = new JTextArea(55, 50);
         receipt.setEditable(false);
-        receipt.setText(cart.generateInvoice(currentUser)); 
+//        receipt.setText(cart.generateInvoice(currentUser)); 
         thank = new JLabel("Thank you for shopping with us!");
 
-        setPreferredSize(new Dimension(284, 382));
-        setLayout(null);
+        mainPanel.add(receipt);
+        mainPanel.add(thank);
 
-        add(receipt);
-        add(thank);
+        receipt.setBounds(30, 50, 212, 280);
+        thank.setBounds(55, 340, 185, 30);
+        
+        this.add(mainPanel);
+    }
 
-        receipt.setBounds(37, 50, 205, 250);
-        thank.setBounds(55, 300, 185, 30);
+    public ShoppingCart getCart() {
+        return cart;
+    }
+
+    public void setCart(ShoppingCart cart) {
+        this.cart = cart;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+    
+    public void initView() {
+        this.setLayout(new FlowLayout());
+        this.setBackground(Color.WHITE);
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(new Point((d.width / 2) - (this.getWidth() / 2), (d.height / 2) - (this.getHeight() / 2)));
+    }
+    
+    public void showReceipt() {
+        receipt.setText(cart.generateInvoice(currentUser)); 
+        this.pack();
+        this.setVisible(true);
     }
 
     public static void main(String[] args) {
 	User currentUser = new Customer("test1234", "Woohoo10101!");
 	ShoppingCart scart = new ShoppingCart(currentUser);
+        
         ProductList plist = ProductFileIO.importProductData();
         scart.addToCart(plist.searchProduct("Apple"), 3);
         scart.addToCart(plist.searchProduct("AOC"), 3);
         JFrame frame = new JFrame("MyPanel");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new ReceiptView(scart, currentUser));
+//        frame.getContentPane().add(new ReceiptView(scart, currentUser));
         frame.pack();
         frame.setVisible(true);
+    }
+    
+    public void addController(CardController controller) {
+        // TODO: Add controllers to respective views.
+        
+    }
+
+    @Override
+    public void update(Observable o, Object selection) {
+        CardModel cm = (CardModel) selection;
+        
+        if(cm.isReceiptFlag()) { // If its customer confirms their check out....
+            this.setCart(cm.getShoppingCart()); // Init shopping cart.
+            this.setCurrentUser(cm.getUsers().getCurrentUser()); // Init current user.
+            this.showReceipt(); // Show frame.
+        } 
     }
 }
