096374c6173aec522fd988005967a4af50d30398
djorange77 <cenzltd2011@gmail.com>
2021-06-02 08:12:05 +1200

    -Added a TableCellRenderer to JTable so data are centre aligned.

--- PDC_Assignment_2/src/com/online/shopping_gui/view/ProductsTableView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/ProductsTableView.java
@@ -7,13 +7,13 @@ import com.online.shopping_gui.model.Table;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 
 /**
  * This class contains the Products Table.
@@ -25,7 +25,7 @@ import javax.swing.ScrollPaneConstants;
  * @since 25/05/2021
  */
 public class ProductsTableView extends JPanel {
-    private final String[] COLUMN_HEADERS = new String[]{"ID", "Product Name", "Price", "Category", "Stock"};
+    private final String[] COLUMN_HEADERS = new String[]{"ID", "Product Name", "Price (NZD $)", "Category", "Stock"};
     private Table table;
     private JTable productTable;
     private JScrollPane scrollPane;
@@ -36,43 +36,23 @@ public class ProductsTableView extends JPanel {
         this.setPreferredSize(new Dimension(710, 350));
         this.setBackground(Color.WHITE);
         ProductList pList = ProductFileIO.importProductData();
-        
-        boolean sorter = true;
+
         table = new Table(pList.convertProductList(), COLUMN_HEADERS); // Init Table Model.
         productTable = new JTable(table);
         productTable.setPreferredScrollableViewportSize(new Dimension(700, 300));
         productTable.setFillsViewportHeight(true);
-        productTable.setAutoCreateRowSorter(sorter);
+        productTable.setAutoCreateRowSorter(true); // Enables sort by column function.
         productTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // Allows user to select only one row at a time.
+        
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer(); // Prep text alignment renderer.
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER); // Define centre alignment.
+        for(int i = 0; i < productTable.getColumnCount(); i++) { // For each column in the productTable.
+            productTable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer); // Set text alignment to centre.
+        }
+            
         scrollPane = new JScrollPane(productTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-//        productTable.addMouseListener(new MouseListener() {
-//            @Override
-//            public void mouseReleased(MouseEvent e) {
-//                
-//            }
-//            
-//            @Override
-//            public void mousePressed(MouseEvent e) {
-//                if(productTable.getSelectedRow() >= 0) {
-//                    String selectedCellValue = productTable.getValueAt(productTable.getSelectedRow() , 1).toString();
-//                    System.out.println(selectedCellValue);
-//                }
-//            }
-//            
-//            @Override
-//            public void mouseExited(MouseEvent e) {
-//            }
-//            
-//            @Override
-//            public void mouseEntered(MouseEvent e) {
-//            }
-//            
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//                itemSelectedLbl.setText("Item Selected: " + productTable.getValueAt(productTable.getSelectedRow() , 1));
-//            }
-//        });
+
         
         this.add(scrollPane);
     }
