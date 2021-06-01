cbbeca3e1a427fec43505e34170308b88cfdd475
djorange77 <cenzltd2011@gmail.com>
2021-06-02 08:07:39 +1200

    -Added a few methods to check the size of the cart. -Added several default methods so that the table sorter can be used correctly. -Added an update method to refresh changes made in object (shopping cart and product list).

--- PDC_Assignment_2/src/com/online/shopping_gui/model/Table.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/model/Table.java
@@ -8,12 +8,11 @@ import javax.swing.table.AbstractTableModel;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 2.0.0
+ * @version 2.0.1
  * @since 18/05/2021
  */
 public class Table extends AbstractTableModel {
 
-//    public String[] column =  new String[]{"ID", "Product Name", "Price", "Category", "Stock"};
     public String[] column;
 
     private Object[][] data;
@@ -22,23 +21,42 @@ public class Table extends AbstractTableModel {
         this.column = column;
         this.data = data;
     }
+    
+    public Object[][] getData() {
+        return data;
+    }
 
-    @Override
-    public int getRowCount() {
-        return this.data.length;
+    public void setData(Object[][] data) {
+        this.data = data;
     }
 
     @Override
     public int getColumnCount() {
         return this.column.length;
     }
+    
+    @Override
+    public int getRowCount() {
+        return this.data.length;
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return column[col];
+    }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         return this.data[rowIndex][columnIndex];
     }
-
-    public String getColumnName(int col) {
-        return column[col];
+    
+    @Override
+    public Class<?> getColumnClass(int c) {
+        return getValueAt(0,c).getClass();
+    }
+    
+    public void update(Object[][] data)
+    {
+        this.setData(data);
     }
 }
