2ac352d141a98c0530706815c842ef64ea03abf1
djorange77 <cenzltd2011@gmail.com>
2021-06-03 14:05:57 +1200

    -Added a text field and its relevant controllers to receive name. -Modified setShipToTxt and setVisa to update when a new user logs in.

--- PDC_Assignment_2/src/com/online/shopping_gui/view/CheckOutView.java
+++ PDC_Assignment_2/src/com/online/shopping_gui/view/CheckOutView.java
@@ -4,13 +4,8 @@ import com.formdev.flatlaf.FlatLightLaf;
 import com.online.shopping_gui.controller.CardController;
 import com.online.shopping_gui.model.CardModel;
 import com.online.shopping_gui.model.Customer;
-import com.online.shopping_gui.model.ProductList;
 import com.online.shopping_gui.model.ShoppingCart;
 import com.online.shopping_gui.model.User;
-import com.online.shopping_gui.model.UserList;
-import com.online.shopping_gui.utilities.AdminDBManager;
-import com.online.shopping_gui.utilities.CustomerDBManager;
-import com.online.shopping_gui.utilities.ProductFileIO;
 import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
@@ -22,16 +17,20 @@ import javax.swing.*;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 2.1.0
+ * @version 2.1.1
  * @since 25/05/2021
  */
 public class CheckOutView extends JFrame implements Observer {
-    public final String INVALID_CARD = "Please enter a valid card number.";
+    public final String INVALID_CARD = "Please enter a valid card no.";
+    public final int PANEL_WIDTH = 284;
+    public final int PANEL_HEIGHT = 382;
     private JPanel mainPanel;
-    private JLabel total, visa, shipTo, ccv, visaCheckLbl;
-    private JTextField totalTxt;
-    private JTextArea shipToTxt;
-    private JPasswordField visaTxt, ccvTxt;
+    private JLabel total, visa, name, address, ccv, visaCheckLbl;
+    private JTextField totalTxt, nameTxt;
+    private JTextArea addressTxt;
+    private JScrollPane scrollPane;
+    private JTextField visaTxt;
+    private JPasswordField ccvTxt;
     private JButton confirm, cancel;
     private ShoppingCart cart;
     private User currentUser;
@@ -47,30 +46,35 @@ public class CheckOutView extends JFrame implements Observer {
         
         mainPanel = new JPanel();
         mainPanel.setLayout(null);
-        mainPanel.setPreferredSize(new Dimension(284, 382));
+        mainPanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
 
         total = new JLabel("Total:");
         totalTxt = new JTextField(20);
         totalTxt.setEditable(false);
-//        totalTxt.setText(String.valueOf(String.format("$%.2f", cart.getGrandTotal())));
-        shipTo = new JLabel("Ship To:");
-        shipToTxt = new JTextArea(100, 100);
-//        setShipToTxt(user);
+        name = new JLabel("Name:");
+        nameTxt = new JTextField(20);
+        nameTxt.setEditable(false);
+        address = new JLabel("Address:");
+        addressTxt = new JTextArea(28, 20);
+        scrollPane = new JScrollPane(addressTxt, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         visa = new JLabel("Card No:");
-        visaTxt = new JPasswordField(20);
+        visaTxt = new JTextField(20);
         visaTxt.setToolTipText("Masked card number.");
         visaCheckLbl = new JLabel("");
         visaCheckLbl.setForeground(Color.RED);
-//        setVisaTxt(user);
         ccv = new JLabel("CCV:");
         ccvTxt = new JPasswordField(5);
         confirm = new JButton("Confirm");
+        confirm.setEnabled(false);
         cancel = new JButton("Cancel");
-
+    
         mainPanel.add(total);
         mainPanel.add(totalTxt);
-        mainPanel.add(shipTo);
-        mainPanel.add(shipToTxt);
+        mainPanel.add(name);
+        mainPanel.add(nameTxt);
+        mainPanel.add(address);
+        mainPanel.add(scrollPane);
         mainPanel.add(visa);
         mainPanel.add(visaTxt);
         mainPanel.add(visaCheckLbl);
@@ -82,8 +86,10 @@ public class CheckOutView extends JFrame implements Observer {
 
         total.setBounds(25, 40, 100, 25);
         totalTxt.setBounds(95, 40, 165, 25);
-        shipTo.setBounds(25, 70, 100, 25);
-        shipToTxt.setBounds(95, 75, 165, 150);
+        name.setBounds(25, 75, 100, 25);
+        nameTxt.setBounds(95, 75, 165, 25);
+        address.setBounds(25, 110, 100, 25);
+        scrollPane.setBounds(95, 110, 165, 115);
         visa.setBounds(25, 235, 100, 25);
         visaTxt.setBounds(95, 235, 165, 25);
         visaCheckLbl.setBounds(95, 260, 165, 15);
@@ -95,14 +101,22 @@ public class CheckOutView extends JFrame implements Observer {
         this.add(mainPanel);
     }
 
-    public JTextArea getShipToTxt() {
-        return shipToTxt;
+    public JTextField getNameTxt() {
+        return nameTxt;
     }
 
-    public JPasswordField getVisaTxt() {
+    public JTextArea getAddressTxt() {
+        return addressTxt;
+    }
+
+    public JTextField getVisaTxt() {
         return visaTxt;
     }
 
+    public JPasswordField getCcvTxt() {
+        return ccvTxt;
+    }
+    
     public JButton getConfirm() {
         return confirm;
     }
@@ -128,17 +142,32 @@ public class CheckOutView extends JFrame implements Observer {
     }
     
     public void setShipToTxt(User currentUser) {
-        if (!((Customer) currentUser).getName().equals("UNKNOWN") && !((Customer) currentUser).getAddress().equals("UNKNOWN")) {
-            shipToTxt.setEditable(false);
-            shipToTxt.setText(((Customer) currentUser).getName()
-                    + "\n" + ((Customer) currentUser).getAddress());
+        nameTxt.setText(((Customer) currentUser).getName());
+        addressTxt.setText(((Customer) currentUser).getAddress()); // Set the text area.
+
+        if (!((Customer) currentUser).getName().equals("UNKNOWN")) {
+            nameTxt.setEditable(false); // If its not unknown, dont allow user to edit.
+        } else { // Otherwise...
+            nameTxt.setText(""); // Clear field
+            nameTxt.setEditable(true); // Let user enter details.
         }
+        
+        if(!((Customer) currentUser).getAddress().equals("UNKNOWN")) {
+            addressTxt.setEditable(false); // If its not unknown, dont allow user to edit.
+        } else { // Otherwise...
+            addressTxt.setText(""); // Clear field
+            addressTxt.setEditable(true); // Let user enter details.
+        }
+        
     }
 
     public void setVisaTxt(User currentUser) {
+        visaTxt.setText(((Customer) currentUser).getCardNumber().replaceAll("\\w(?=\\w{4})", "*")); // Mask everything except the last 4 digits of the card number.
         if (!((Customer) currentUser).getCardNumber().equals("UNKNOWN")) {
-            visaTxt.setEditable(false);
-            visaTxt.setText(((Customer) currentUser).getCardNumber());
+            visaTxt.setEditable(false); // If its not unknown, dont allow user to edit.
+        } else {
+            visaTxt.setText(""); // Clear field
+            visaTxt.setEditable(true); // Let user enter details.
         }
     }
     
@@ -146,10 +175,11 @@ public class CheckOutView extends JFrame implements Observer {
         this.setLayout(new FlowLayout());
         this.setBackground(Color.WHITE);
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(new Point((d.width / 2) - (this.getWidth() / 2), (d.height / 2) - (this.getHeight() / 2)));
+        this.setLocation(new Point((d.width / 3) - (this.getWidth() / 2), (d.height / 3) - (this.getHeight() / 2)));
     }
     
     public void showCheckOut() {
+        System.out.println("Current User: " + this.currentUser.toString());
         totalTxt.setText(String.valueOf(String.format("$%.2f", this.cart.getGrandTotal())));
         setShipToTxt(this.currentUser);
         setVisaTxt(this.currentUser);
@@ -164,25 +194,18 @@ public class CheckOutView extends JFrame implements Observer {
     public void passVisaCheck() {
         visaCheckLbl.setText("");
     }
-
-    public static void main(String[] args) {
-        UserList users = new UserList(CustomerDBManager.importData(), AdminDBManager.importData());
-        User currentUser = users.searchUser("djorange77");
-        ShoppingCart scart = new ShoppingCart(currentUser);
-        ProductList plist = ProductFileIO.importProductData();
-        scart.addToCart(plist.searchProduct("Apple"), 3);
-        scart.addToCart(plist.searchProduct("AOC"), 3);
-        JFrame frame = new CheckOutView();
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    
+    public void configConfirmBtn(boolean value) {
+        confirm.setEnabled(value);
     }
     
     public void addController(CardController controller) {
         // TODO: Add controllers to respective views.
-        shipToTxt.getDocument().addDocumentListener(controller);
+        nameTxt.getDocument().addDocumentListener(controller);
+        addressTxt.getDocument().addDocumentListener(controller);
         visaTxt.getDocument().addDocumentListener(controller);
+        ccvTxt.getDocument().addDocumentListener(controller);
+        ccvTxt.addKeyListener(controller);
         confirm.addActionListener(controller);
         cancel.addActionListener(controller);
     }
@@ -193,10 +216,17 @@ public class CheckOutView extends JFrame implements Observer {
         
         if(cm.isCheckOutFlag()) { // If its customer check out....
             this.setCart(cm.getShoppingCart()); // Init shopping cart.
-            this.setCurrentUser(cm.getUsers().getCurrentUser()); // Init current user.
+            this.setCurrentUser(cm.getShoppingCart().getUser()); // Init current user.
             this.showCheckOut(); // Show frame.
         } else if(cm.isReceiptFlag()) { // If receipt flag is triggered...
             this.dispose(); // Close this window.
+        } else if(cm.isCustLoginFlag()) { // If a different customer is logged in...
+            this.dispose();
+            this.setCart(cm.getShoppingCart()); // Re-init shopping cart.
+            this.setCurrentUser(cm.getShoppingCart().getUser()); // Init current user.
+            setShipToTxt(this.currentUser);
+            scrollPane.updateUI();
+            setVisaTxt(this.currentUser);
         }
     }
 }
